c323bf90f4173afb944d419ff0ff874098017cec
vamsi-krishna007 <67186572+vamsi-krishna007@users.noreply.github.com>
2023-08-22 16:15:23 +0530

    Update PrimeNumber.java

--- 1. Numbers/PrimeNumber.java
+++ 1. Numbers/PrimeNumber.java
@@ -23,17 +23,15 @@ public class PrimeNumber {
 	}
 	
 	static boolean isPrime(int number) {
-		
 		if (number <= 1) {
-            return false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(number); i++) {
-                if (number % i == 0) {
-                    return false;
-                }
-            }
-        }
+            		return false;
+        	} else {
+            		for (int i = 2; i <= Math.sqrt(number); i++) {
+                		if (number % i == 0) {
+                    			return false;
+                		}
+            		}
+        	}
 		return true;
 	}
-
 }
